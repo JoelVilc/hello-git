f08211c4c5ee03f851a9d2c48d388aa3fad18c7d
jvilchezv <jvilchezv02@gmail.com>
2024-07-12 23:04:06 -0500

    Declarando un función suma

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,14 @@
 print("Hello Git")
+
+def main (*args):
+    par = 0
+    impar = 0
+    for i in args:
+        if i%2 == 0:
+            par += i
+        else:
+            impar += i
+    return par, impar
+
+par, impar = main(3,4,5,6,7)
+print(f"Par:{par}, Impar:{impar}")
